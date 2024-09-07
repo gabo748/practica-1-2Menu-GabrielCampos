4b033b8c52cd3c33eebf42b7dc380d82fdefba04
Gabriel Campos <gaeducampos@gmail.com>
2024-09-07 14:51:29 -0600

    finished

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import {CalculadoraComponent} from "./components/calculadora/calculadora.component";
+import {AppComponent} from "./app.component";
+import {Practica2Component} from "./pages/practica2/practica2.component";
+import {Practica1Component} from "./pages/practica1/practica1.component";
 
-const routes: Routes = [];
+
+const routes: Routes = [
+  { path: 'pages/practica-2', component: Practica2Component },  // Route to CalculadoraComponent
+  { path: 'pages/practica-1', component: Practica1Component }
+];
+
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
